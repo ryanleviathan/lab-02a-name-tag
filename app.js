01581a0fb95b7123a750842a5439061bc7235a95
ryanleviathan <ryan.m.carreras@gmail.com>
2020-09-23 16:44:54 -0700

    initial button add commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const input = document.getElementById('name')
+const button = document.getElementById('nameButton')
+const pinkButton = document.getElementById('pinkButton')
+const lightGreenButton = document.getElementById("lightGreenButton")
+const lightGlueButton = document.getElementById('lightBlueButton')
+
+button.addEventListener('click', () => {
+
+})
+
+pinkButton.addEventListener('click', () => {
+    
+})
+
+lightGreenButton.addEventListener('click', () => {
+    
+})
+
+lightBlueButton.addEventListener('click', () => {
+    
+})
