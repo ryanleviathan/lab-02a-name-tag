4187ded105dadf4d507ec9dcb33c06db94e0b233
ryanleviathan <ryan.m.carreras@gmail.com>
2020-09-24 11:25:40 -0700

    change name now works

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-const input = document.getElementById('name')
+const input = document.getElementById('input')
+const output = document.getElementById('insert-name')
 const button = document.getElementById('nameButton')
 const pinkButton = document.getElementById('pinkButton')
 const lightGreenButton = document.getElementById("lightGreenButton")
 const lightGlueButton = document.getElementById('lightBlueButton')
 
 button.addEventListener('click', () => {
-
+    const value = input.value;
+    output.textContent = value;
 })
 
-pinkButton.addEventListener('click', () => {
-    
-})
-
-lightGreenButton.addEventListener('click', () => {
-    
-})
-
-lightBlueButton.addEventListener('click', () => {
-    
-})
